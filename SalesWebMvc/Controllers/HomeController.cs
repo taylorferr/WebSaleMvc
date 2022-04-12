ed044059ba1488076bd975b172e32fabbd7e7583
taylor.pereira <taylor.pereira@southsystem.com.br>
2022-04-12 14:16:38 -0300

    Fisrt controller and Razor pages testes

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from c# Course.";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
